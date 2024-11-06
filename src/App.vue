3d102c31ac9785097b9d5f13108f9266d7265ad3
BachLester <bachlester0@gmail.com>
2024-11-06 20:17:34 +0800

    独立维护仓库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from './stores/index'
+
+const userStore = useUserStore()
+</script>
 
 <template>
-  <div>I am App</div>
+  <div>
+    I am App
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('hahaha周子颜是我的性奴')"
+      >登录</el-button
+    >
+    <el-button @click="userStore.removeToken">退出</el-button>
+  </div>
 </template>
 
 <style scoped></style>
